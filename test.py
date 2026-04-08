03a35a69351d945e5b7a75b35888ba35a257017a
akshat <aj1508109@gmail.com>
2026-04-08 10:20:00 +0530

    testing git hooks

---
+++ test.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    return a + b
+
+
+def divide(a, b):
+    return a / b
+
+
+def main():
+    result = add(2, 3)
+    #print("Result is:", result)  # ❌ will fail your print check
+
+
+if __name__ == "__main__":
+    main()
